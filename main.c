0f659f88367ca89a1a7a3b94cfb9fa670fbc4fd1
Luiz Otavio <luizotrv@gmail.com>
2017-05-14 15:37:00 -0300

    calc max flux @TODO: solve realloc problem

--- main.c
+++ main.c
@@ -26,6 +26,7 @@ int breadthFirstSearch(Tree *tree, int tree_index, int recursion_index, int ** p
 	if(tree[tree_index].edges_size){
 		if(recursion_index)
 			for(i = 0; i < tree[tree_index].edges_size; i++){
+				if(!tree[tree_index].edges[i].size) continue;
 				to = tree[tree_index].edges[i].to;
 				r_value = breadthFirstSearch(tree, to, recursion_index-1, path);
 				if(r_value == to && path != NULL){
@@ -43,18 +44,46 @@ int getMinValue(Tree *tree, int size, int **path){
 		index = (*path)[i-1];
 		b_index = (*path)[i];
 		for(j = 0; j < tree[b_index].edges_size; j++) {
-			if(tree[b_index].edges[j].to == index){
+			if(tree[b_index].edges[j].size && tree[b_index].edges[j].to == index){
 				min_value = tree[b_index].edges[j].size < min_value || min_value == -1 ? tree[b_index].edges[j].size : min_value;
-				continue;
 			}
 		}
 	}
 	return min_value;
 }
 
+void setGraphWithMinValues(Tree *tree, int size, int **path, int min_value){
+	int i, j, k, index, b_index, edge_index;
+	for(i = size; i; i--){
+		index = (*path)[i-1];
+		b_index = (*path)[i];
+		for(j = 0; j < tree[b_index].edges_size; j++) {
+			if(tree[b_index].edges[j].to == index){
+				tree[b_index].edges[j].size -= min_value;
+				//create new edge
+				edge_index = tree[index].edges_size;
+				for(k = 0; k < edge_index; k++){
+					if(tree[index].edges[k].to == b_index){
+						edge_index = k;
+						break;
+					}
+				}
+				if(edge_index < tree[index].edges_size){
+					tree[index].edges[ edge_index ].size += min_value;
+				} else {
+					tree[index].edges_size++;
+					//tree[index].edges = realloc(tree[index].edges, (size_t)(++tree[index].edges_size) * sizeof(int));
+					tree[index].edges[ edge_index ].to = b_index;
+					tree[index].edges[ edge_index ].size = min_value;
+				}
+			}
+		}
+	}
+}
+
 int main() {
 	int V, E, F, C, u, v, m;
-	int i, j, k, l, min_value;
+	int i, j, min_value;
 	int *path = NULL;
 	int edge_index;
 	scanf("%d %d %d %d", &V, &E, &F, &C);
@@ -62,11 +91,14 @@ int main() {
 	for(i = 0; i < V; i++) {
 		tree[i].type = TYPE_EMPTY;
 		tree[i].edges_size = 0;
+		//tree[i].edges = NULL;
+		tree[i].edges = malloc(1000*sizeof(int));
 	}
 	for(i = 0; i < E; i++) {
 		scanf("%d %d %d", &u, &v, &m);
 		edge_index = tree[u].edges_size;
-		tree[u].edges = realloc( tree[u].edges, (size_t)++tree[u].edges_size * sizeof(Edge) );
+		//tree[u].edges = realloc( tree[u].edges, (size_t)++tree[u].edges_size * sizeof(Edge) );
+		tree[u].edges_size++;
 		tree[u].edges[ edge_index ].to = v;
 		tree[u].edges[ edge_index ].size = m;
 	}
@@ -79,33 +111,19 @@ int main() {
 		tree[u].type = TYPE_CLIENT;
 	}
 
+	int total = 0;
 	for(i = 0; i < V; i++) {
 		for(j = 1; j <= V; j++) {
 			if(tree[i].type == TYPE_FRANCHISE) {
 				if(breadthFirstSearch(tree, i, j, &path) != -1){
 					min_value = getMinValue(tree, j, &path);
-
-					for(k = 1; k < j; k++) {
-						for(l = 0; l < tree[k].edges_size; l++) {
-							if(tree[k-1].edges[l].to == k){
-								tree[k-1].edges[l].size =- min_value;
-								if(tree[k-1].edges[l].size){
-									edge_index = tree[k-1].edges_size;
-									tree[k-1].edges = realloc(tree[k-1].edges, (size_t)++tree[k-1].edges_size * sizeof(int));
-									tree[k-1].edges[ edge_index ].to = k;
-									tree[k-1].edges[ edge_index ].size = min_value;
-								}else{
-
-								}
-							}
-						}
-					}
-					printf("asd");
+					total+=min_value;
+					setGraphWithMinValues(tree, j, &path, min_value);
 				}
 			}
 		}
 	}
-
+	printf("%d", total);
 	return EXIT_SUCCESS;
 }
 
