5e79d9a8183ac4ec05cebcd0ef925b6f3d949ee8
Luiz Otavio <luizotrv@gmail.com>
2017-05-22 22:45:47 -0300

    main comments

--- main.c
+++ main.c
@@ -4,10 +4,14 @@
 #include "tools.h"
 
 int main() {
+
+	//init variables
 	int V, E, F, C, u, v, m;
 	int i, j, min_value;
 	int *path = NULL;
 	int edge_index;
+
+	// Read from STDIN and store on variables
 	scanf("%d %d %d %d", &V, &E, &F, &C);
 	Tree *tree = malloc(V * sizeof(Tree));
 	for(i = 0; i < V; i++) {
@@ -18,10 +22,13 @@ int main() {
 	for(i = 0; i < E; i++) {
 		scanf("%d %d %d", &u, &v, &m);
 		edge_index = tree[u].edges_size;
+		//mount graph from STDIN
 		tree[u].edges = realloc( tree[u].edges, (size_t)(++tree[u].edges_size) * sizeof(Edge) );
 		tree[u].edges[ edge_index ].to = v;
 		tree[u].edges[ edge_index ].size = m;
 	}
+
+	//set FRANCHISE's and CLIENT's types
 	for(i = 0; i < F; i++) {
 		scanf("%d", &u);
 		tree[u].type = TYPE_FRANCHISE;
@@ -31,23 +38,34 @@ int main() {
 		tree[u].type = TYPE_CLIENT;
 	}
 
-	int total = 0;
+	int total = 0; 	//final result
+
+	//principal loop
 	for(i = 0; i < V; i++) {
 		if(tree[i].type == TYPE_FRANCHISE) {
 			for(j = 1; j < V; j++) {
+				// here, 'j' is our recursion index and path length
 				if( breadthFirstSearch(tree, i, j, &path, j) != -1 ){
+					// here, we have get the path from graph
 					min_value = getMinValue(tree, j, &path);
-					total += min_value;
+					// then, we get the minimal value from all edges
+					total += min_value; // sum the result
+					// set the graph with backwards edges
 					setGraphWithMinValues(tree, j, &path, min_value);
+
+					// clear path
 					if(path != NULL){
 						free(path);
 						path = NULL;
 					}
+					// back recursion index
 					j--;
 				}
 			}
 		}
 	}
+
+	//free allocated memory
 	for(i = 0; i < V; i++) free(tree[i].edges);
 	free(tree);
 	free(path);
