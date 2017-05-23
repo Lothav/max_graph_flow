7188f3ab76904d51a7d5252ff9478da55af88f83
Luiz Otavio <luizotrv@gmail.com>
2017-05-22 22:49:10 -0300

    tools comments

--- tools.c
+++ tools.c
@@ -5,16 +5,21 @@
 #include "tools.h"
 
 int getMinValue(Tree *tree, int size, int **path){
+	//init values
 	int min_value = -1, i, j, index, b_index;
+	// browse the vector vertex's
 	for(i = size; i; i--){
 		index = (*path)[i-1];
 		b_index = (*path)[i];
+		// browse the vector edges on each vertex
 		for(j = 0; j < tree[b_index].edges_size; j++) {
+			// check if value is lower than min_value
 			if(tree[b_index].edges[j].size && tree[b_index].edges[j].to == index){
 				min_value = tree[b_index].edges[j].size < min_value || min_value == -1 ? tree[b_index].edges[j].size : min_value;
 			}
 		}
 	}
+	// return min value
 	return min_value;
 }
 
--- tools.h
+++ tools.h
@@ -6,5 +6,7 @@
 #define MAX_GRAPH_FLOW_TOOLS_H
 #include "graph.h"
 
+// return the minimal value edge on the path
 int getMinValue(Tree *tree, int size, int **path);
+
 #endif //MAX_GRAPH_FLOW_TOOLS_H
